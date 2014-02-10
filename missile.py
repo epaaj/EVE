2560f4fdb4961699b8ba8d073925dbb589085434
Jimmy Garpehäll <epaaj@ninjaloot.se>
2014-02-10 23:15:59 +0100

    Added damage per second calculations. Fixed typo in option parser.

--- missile.py
+++ missile.py
@@ -8,13 +8,15 @@ parser.add_option("-c", "--cruise-missile",
         action="store_true", dest="cruise_missile", default=False,
         help="Show Cruise Missile stats")
 
-(options, args) = parser.parser_args()
+(options, args) = parser.parse_args()
 
 class Missile_bonus(object):
     def __init__(self):
         # Skills
         # Cruise missile damage: 5%
         self.cruise_missile = 4
+        # Heavy assault missule damage: 5%
+        self.heavy_assault_missile = 4
         # Signature radius factor for missile explosions: -5%
         self.guided_missile_precision = 3
         # Maximum flight time: 10%
@@ -56,7 +58,7 @@ class Missile_bonus(object):
         self.large_warhead_flare_catalyst_I = 0
         self.large_warhead_flare_catalyst_II = 0
         # Explosion radius: I: -15%; II: -20%
-        self.large_warhead_rigor_catalyst_I = 3
+        self.large_warhead_rigor_catalyst_I = 0
         self.large_warhead_rigor_catalyst_II = 0
 
     def stacking_penalty(self, n):
@@ -82,12 +84,13 @@ class Missile_bonus(object):
         return vel_bonus
 
 class Missile(object):
-    def __init__(self, name, damage, radius, velocity, reduction):
+    def __init__(self, name, damage, radius, velocity, reduction, rate_of_fire):
         self.name = name
         self.damage = damage
         self.radius = radius
         self.velocity = velocity
         self.reduction = reduction
+        self.rate_of_fire = rate_of_fire
 
     def __str__(self):
         base = "{0.name:<30}{0.damage} / {0.radius} / {0.velocity} {0.reduction}"
@@ -106,14 +109,16 @@ class Missile(object):
             )
         )
         effect = (damage / math.floor(self.damage * dmg_bonus)) * 100
-        return damage, effect, sdf, vdf
+        dps = (damage / self.rate_of_fire)
+        return damage, effect, sdf, vdf, dps
 
 bonus = Missile_bonus()
 missiles = []
-missiles.append(Missile("Cruise Missile", 300, 300, 69, 4.5))
-missiles.append(Missile("Caldari Navy Cruise Missile", 345, 300, 69, 4.5))
-missiles.append(Missile("Precision Cruise Missile", 260, 270, 71, 3.5))
-missiles.append(Missile("Fury Cruise Missile", 384, 550, 58, 4.7))
+missiles.append(Missile("Cruise Missile", 300, 300, 69, 4.5, 16.55))
+missiles.append(Missile("Caldari Navy Cruise Missile", 345, 300, 69, 4.5, 16.55))
+missiles.append(Missile("Precision Cruise Missile", 260, 270, 71, 3.5, 16.55))
+missiles.append(Missile("Fury Cruise Missile", 384, 550, 58, 4.7, 16.55))
+missiles.append(Missile("Heavy Assault Missile", 100, 125, 101, 4.5, 6.4))
 
 for missile in missiles:
     print(missile)
@@ -125,14 +130,14 @@ def damage_matrix(ship, signature, vt):
     print("{0:<30}{1}m, {2}m/s".format(ship, signature, vt))
 
     for missile in missiles:
-        damage, effect, sdf, vdf = missile.calculate_damage(signature, vt)
+        damage, effect, sdf, vdf, dps = missile.calculate_damage(signature, vt)
         print(
-            "{0:<30}{1:>3} hp ({2:>6.2f}%) (SigF: {3:.2f}, VelF: {4:.2f})".format(
-                missile.name, damage, effect, sdf, vdf
+                "{0:<30}{1:>3} hp ({2:>6.2f}%) (SigF: {3:.2f}, VelF: {4:.2f}, DPS: {5:>5.2f})".format(
+                missile.name, damage, effect, sdf, vdf, dps
             )
         )
 
-        damage, effect, sdf, vdf = missile.calculate_damage(
+        damage, effect, sdf, vdf, dps = missile.calculate_damage(
             signature,
             vt,
             bonus.damage_bonus(),
@@ -140,8 +145,8 @@ def damage_matrix(ship, signature, vt):
             bonus.velocity_bonus()
         )
         print(
-            "{0:<3}{1:<27}{2:>3} hp ({3:>6.2f}%) (SigF: {4:.2f}, VelF: {5:.2f})".format(
-                "", "with bonus:", damage, effect, sdf, vdf
+            "{0:<3}{1:<27}{2:>3} hp ({3:>6.2f}%) (SigF: {4:.2f}, VelF: {5:.2f}, DPS: {6:>5.2f})".format(
+                "", "with bonus:", damage, effect, sdf, vdf, dps
             )
         )
 
